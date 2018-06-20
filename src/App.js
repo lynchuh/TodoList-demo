de3873b5285bfcd01b4648643fe0161e976aed38
lynchuh <632955934@qq.com>
2018-06-20 20:40:41 +0800

    更新样式

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
         <div className="inputWrapper">
           <TodoInput content={this.state.newTodo}/>
         </div>
-        <ol>
+        <ol className="todoList">
           {todos}
         </ol>
       </div>
