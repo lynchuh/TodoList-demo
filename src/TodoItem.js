74710e1f0e4c6fbca3df209ba4d5f88142dedff7
lynchuh <632955934@qq.com>
2018-06-23 18:03:27 +0800

    更新todoItem组件样式

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,18 +1,20 @@
 import React,{Component} from 'react'
+import './TodoItem.css'
 
 export default class TodoItem extends Component{
     render(){
         return (
-            <div>
+            <div className="todoItem">
                 <input type="checkbox" checked={this.props.todo.status==='completed'}
                     onChange={this.toggle.bind(this)}/>
-                    {this.props.todo.title}
+                <span className="title">{this.props.todo.title}</span>
                 <button onClick={this.delete.bind(this)}>删除</button> 
             </div>
         )
     }
     toggle(e){
         this.props.onToggle(e,this.props.todo)
+        console.log(this.props.todo.status)
     }
     delete(e){
         this.props.onDelete(e,this.props.todo)
